cc889190f8abb899d435e732193b57596966b5b2
Mark Thomas <markbt@efaref.net>
2020-01-19 23:05:07 +0000

    remove vestigial line-drawing regex
    The line-drawing escape sequence regex is no longer needed, as line-drawing
    escape sequences are parsed by termwiz.

--- src/line_drawing.rs
+++ src/line_drawing.rs
@@ -7,9 +7,6 @@
 //! Handle this by converting characters between escape sequence blocks
 //! into the equivalent unicode character.
 
-use lazy_static::lazy_static;
-use regex::Regex;
-
 // Start replacing bytes after 0x5F.
 const REPLACEMENTS_START: usize = 0x5F;
 
@@ -18,11 +15,6 @@ const UNICODE_REPLACEMENTS: &[&str] = &[
     "\u{A0}", "◆", "▒", "␉", "␌", "␍", "␊", "°", "±", "␤", "␋", "┘", "┐", "┌", "└", "┼", "⎺", "⎻",
     "─", "⎼", "⎽", "├", "┤", "┴", "┬", "│", "≤", "≥", "π", "≠", "£", "·",
 ];
-
-lazy_static! {
-    /// Regex for detecting start and end escape sequences.
-    pub(crate) static ref ESCAPE_SEQUENCE: Regex = Regex::new("\x1B\\([0B]").unwrap();
-}
 
 pub(crate) fn convert_line_drawing(input: &str) -> String {
     let mut out = String::with_capacity(input.len());
@@ -44,6 +36,6 @@ mod test {
     #[test]
     fn test_convert_line_drawing() {
         assert_eq!(convert_line_drawing("aaaaa"), "▒▒▒▒▒");
-        assert_eq!(convert_line_drawing("tqutqu"), "├─┤├─┤");
+        assert_eq!(convert_line_drawing("tquOKtqu"), "├─┤OK├─┤");
     }
 }
